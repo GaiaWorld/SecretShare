a33b245f53a4009f29531fd6e714d7ae72bb1c91
jayphbee <jayphbee@163.com>
2018-11-15 12:08:11 +0800

    Convert btc wif key to private key

--- shares.js
+++ shares.js
@@ -1,5 +1,7 @@
-const secrets = require("secrets.js")
+const secrets = require("secrets.js");
 const readline = require('readline');
+const wif = require("wif");
+const b58c = require("base58check")
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,15 +10,38 @@ const rl = readline.createInterface({
 
 
 rl.question('输入密钥，分割份数，门限，以一个空格隔开: ', (input) => {
-  const inputs = input.split(" ");  
-  var pk = inputs[0];
+  const inputs = input.split(" ");
+  var sk = inputs[0];
   var sh = inputs[1];
   var th = inputs[2];
 
-  var shares = secrets.share(pk, parseInt(sh), parseInt(th));
+  try {
+    // btc
+    b58c.decode(sk) // just test if sk is base58check
+    sk = wif.decode(sk).privateKey;
+    sk = byteToHexString(sk);
+    var shares = secrets.share(sk, parseInt(sh), parseInt(th));
+  } catch (_e) {
+    // eth
+    shares = secrets.share(sk, parseInt(sh), parseInt(th));
+  }
 
   console.log(shares)
 
   rl.close();
 });
 
+function byteToHexString(uint8arr) {
+  if (!uint8arr) {
+    return '';
+  }
+
+  var hexStr = '';
+  for (var i = 0; i < uint8arr.length; i++) {
+    var hex = (uint8arr[i] & 0xff).toString(16);
+    hex = (hex.length === 1) ? '0' + hex : hex;
+    hexStr += hex;
+  }
+
+  return hexStr.toLocaleLowerCase();
+}
